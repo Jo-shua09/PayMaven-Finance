4bf71f0234f134c38d124a0fd9181cbf84609d47
jo-shua09 <joshuaonyeka039@gmail.com>
2025-05-12 22:49:17 +0100

    completed the dummy data and ready to build

--- src/assets/dummyData.ts
+++ src/assets/dummyData.ts
@@ -1,17 +1,47 @@
-export const Prices = [
+interface PricesProps {
+	id: number;
+	type: string;
+	name: string;
+	intro: string;
+	service: string;
+	payType: string;
+	payDura: string;
+	desc: string[];
+	buttonText: string;
+}
+
+export const Prices: PricesProps[] = [
 	{
 		id: 1,
 		type: "",
 		name: "personal",
 		intro: "Perfect for side or hobby projects",
 		service: "4%",
-        payType:"service fee",
-        payDura:"payment",
-        desc:{
-            "Process Unlimited Payments",
-            "Activate Three Business Services",
-            "Analyze and Predict Financial Trends",
-        },
-        buttonText:"get started",
+		payType: "service fee",
+		payDura: "payment",
+		desc: ["Process Unlimited Payments", "Activate Three Business Services", "Analyze and Predict Financial Trends"],
+		buttonText: "get started",
+	},
+	{
+		id: 2,
+		type: "popular",
+		name: "start up",
+		intro: "Perfect for small teams",
+		service: "5%",
+		payType: "service fee",
+		payDura: "payment",
+		desc: ["Process Unlimited Payments", "Activate Three Business Services", "Analyze and Predict Financial Trends"],
+		buttonText: "get started",
+	},
+	{
+		id: 1,
+		type: "",
+		name: "organization",
+		intro: "Perfect for organizations",
+		service: "$150",
+		payType: "",
+		payDura: "month",
+		desc: ["Process Unlimited Payments", "Activate Three Business Services", "Analyze and Predict Financial Trends"],
+		buttonText: "get started",
 	},
 ];
