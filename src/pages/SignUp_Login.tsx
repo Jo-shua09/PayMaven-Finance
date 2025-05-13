d0533047de5e3afab1aa12b3d1e5833b3f67d4a2
jo-shua09 <joshuaonyeka039@gmail.com>
2025-05-13 21:40:38 +0100

    started the sign up page

---
+++ src/pages/SignUp_Login.tsx
@@ -0,0 +1,12 @@
+const SignUp_Login = () => {
+	return (
+		<div className="w-full">
+			<div className="w-full scetion-sec flex items-center justify-between gap-10 flex-wrap sm:flex-nowrap">
+				<div className=""></div>
+				<div className="sm:flex hidden w-full h-full"></div>
+			</div>
+		</div>
+	);
+};
+
+export default SignUp_Login;
--- src/routes.tsx/AppRouter.tsx
+++ src/routes.tsx/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "../layouts/AppLayout";
 import LandingPage from "../pages/LandingPage";
+import SignUp_Login from "../pages/SignUp_Login";
 
 const AppRouter = () => {
 	return (
@@ -8,6 +9,8 @@ const AppRouter = () => {
 			<Routes>
 				<Route element={<AppLayout />}>
 					<Route path="/" element={<LandingPage />} />
+					<Route path="/login" element={<SignUp_Login />} />
+					<Route path="/sign-up" element={<SignUp_Login />} />
 				</Route>
 			</Routes>
 		</Router>
