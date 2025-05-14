565492cf8c92f98d72e43ae3c8d6f642dc33b3bb
jo-shua09 <joshuaonyeka039@gmail.com>
2025-05-14 21:43:47 +0100

    here now

--- src/pages/SignUp_Login.tsx
+++ src/pages/SignUp_Login.tsx
@@ -1,15 +1,18 @@
 import { useLocation } from "react-router-dom";
+import signImage from "/src/assets/images/signup_cover.jpg";
 
 const SignUp_Login = () => {
 	const location = useLocation();
 
 	return (
 		<div className="w-full">
-			<div className="w-full section-sec flex items-center justify-between gap-10 flex-wrap sm:flex-nowrap">
-				<div className="">
+			<div className="w-full section-sec  flex items-center justify-between gap-10 flex-wrap sm:flex-nowrap">
+				<div className="w-full md:flex-1 h-screen">
 					{location.pathname == "/sign-up" ? (
 						<div data-aos="fade-right" className="w-full">
-							<div className="space-y-10">hello</div>
+							<div className="space-y-10">
+								<h2 className=""></h2>
+							</div>
 							<form className="w-full m-auto text-center"></form>
 						</div>
 					) : (
@@ -17,8 +20,8 @@ const SignUp_Login = () => {
 					)}
 				</div>
 
-				<div className="sm:flex hidden w-full h-full">
-					<img src="" alt="Get started image" className="w-full h-full object-cover" />
+				<div className="sm:flex hidden w-full h-full md:flex-1">
+					<img src={signImage} alt="Get started image" className="w-full h-full object-cover" />
 				</div>
 			</div>
 		</div>
