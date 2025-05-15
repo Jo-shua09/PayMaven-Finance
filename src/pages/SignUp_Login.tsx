23250d694cb08ed7335dafd42ac53f7134f9eb0a
jo-shua09 <joshuaonyeka039@gmail.com>
2025-05-15 21:47:39 +0100

    refactor: clean up Firebase component and enhance layout in SignUp_Login

--- src/pages/SignUp_Login.tsx
+++ src/pages/SignUp_Login.tsx
@@ -17,7 +17,7 @@ const SignUp_Login = () => {
 			<div className="w-full md:section -mt-20 md:-mt-10 !px-[3%] min-h-screen h-full justify-center flex items-center gap-20 flex-wrap sm:flex-nowrap">
 				<div className="md:w-1/3 w-full">
 					{location.pathname == "/sign-up" ? (
-						<div data-aos="fade-right" className="w-full max-h-screen">
+						<div data-aos="fade-right" className="w-full max-h-screen overflow-y-hidden">
 							<div className="flex flex-col items-center justify-center place-content-center">
 								<div className="flex items-center gap-x-3 mb-16">
 									<img src="logo.svg" alt="logo" className="w-16 h-16" />
@@ -113,7 +113,7 @@ const SignUp_Login = () => {
 							</div>
 						</div>
 					) : (
-						<div data-aos="fade-left" className="w-full translate-x-0">
+						<div data-aos="fade-right" className="w-full max-h-screen overflow-y-hidden">
 							<div className="flex flex-col items-center justify-center place-content-center">
 								<div className="flex items-center gap-x-3 mb-16">
 									<img src="logo.svg" alt="logo" className="w-16 h-16" />
@@ -197,7 +197,7 @@ const SignUp_Login = () => {
 					)}
 				</div>
 
-				<div className="sm:flex hidden w-[55rem] h-full">
+				<div className="md:flex hidden w-[55rem] h-full">
 					<img
 						data-aos="fade-left"
 						loading="lazy"
